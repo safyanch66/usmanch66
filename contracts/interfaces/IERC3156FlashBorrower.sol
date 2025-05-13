0907316cd594b4d155a011a9c053a98f9e6692d1
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:00:28 +0500

    Update IERC3156FlashBorrower.sol

--- contracts/interfaces/IERC3156FlashBorrower.sol
+++ contracts/interfaces/IERC3156FlashBorrower.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (interfaces/IERC3156FlashBorrower.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 /**
  * @dev Interface of the ERC-3156 FlashBorrower, as defined in
