b6c9e54bfe10a93503f31884c5aefefe1f8574cb
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:05:03 +0500

    Update IERC5805.sol

--- contracts/interfaces/IERC5805.sol
+++ contracts/interfaces/IERC5805.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC5805.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IVotes} from "../governance/utils/IVotes.sol";
 import {IERC6372} from "./IERC6372.sol";
