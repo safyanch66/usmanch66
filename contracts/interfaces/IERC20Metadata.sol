4b4ec9af9b832e3d3bb042448ce7dce44f00945e
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:57:59 +0500

    Update IERC20Metadata.sol

--- contracts/interfaces/IERC20Metadata.sol
+++ contracts/interfaces/IERC20Metadata.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC20Metadata.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IERC20Metadata} from "../token/ERC20/extensions/IERC20Metadata.sol";
