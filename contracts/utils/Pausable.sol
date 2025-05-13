f00cc38d8c49200625052a609a38adce1745c3fa
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:49:50 +0500

    Update Pausable.sol

--- contracts/utils/Pausable.sol
+++ contracts/utils/Pausable.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (utils/Pausable.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {Context} from "../utils/Context.sol";
 
