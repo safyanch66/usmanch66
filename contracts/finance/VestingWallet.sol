440f9d4e4ba688fcd6074b36da8812fd553b194a
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:42:28 +0500

    Update VestingWallet.sol

--- contracts/finance/VestingWallet.sol
+++ contracts/finance/VestingWallet.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (finance/VestingWallet.sol)
-pragma solidity ^0.8.20;
+
+pragma solidity ^0.8.10;
 
 import {IERC20} from "../token/ERC20/IERC20.sol";
 import {SafeERC20} from "../token/ERC20/utils/SafeERC20.sol";
