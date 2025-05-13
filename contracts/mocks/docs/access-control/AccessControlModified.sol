56f9b65b0c80d05803d63dbc4387bfc609078dac
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:18:30 +0500

    Update AccessControlModified.sol

--- contracts/mocks/docs/access-control/AccessControlModified.sol
+++ contracts/mocks/docs/access-control/AccessControlModified.sol
@@ -1,6 +1,6 @@
 // contracts/AccessControlModified.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {AccessControl} from "../../../access/AccessControl.sol";
 
