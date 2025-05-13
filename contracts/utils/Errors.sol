84b5b381fe85189fed77845439c33b762146bcc6
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:48:17 +0500

    Update Errors.sol

--- contracts/utils/Errors.sol
+++ contracts/utils/Errors.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (utils/Errors.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 /**
  * @dev Collection of common custom errors used in multiple contracts
