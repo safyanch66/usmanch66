7195c12296dc5576e1a2413dd76b2d46dc453087
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:45:43 +0500

    Update ReentrancyTransientMock.sol

--- contracts/mocks/ReentrancyTransientMock.sol
+++ contracts/mocks/ReentrancyTransientMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.14;
 
 import {ReentrancyGuardTransient} from "../utils/ReentrancyGuardTransient.sol";
 import {ReentrancyAttack} from "./ReentrancyAttack.sol";
