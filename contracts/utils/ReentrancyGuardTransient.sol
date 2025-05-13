21e374b28086f269b7b8c0b38a6ad7bbdadf1264
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:50:27 +0500

    Update ReentrancyGuardTransient.sol

--- contracts/utils/ReentrancyGuardTransient.sol
+++ contracts/utils/ReentrancyGuardTransient.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (utils/ReentrancyGuardTransient.sol)
 
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.14;
 
 import {TransientSlot} from "./TransientSlot.sol";
 
