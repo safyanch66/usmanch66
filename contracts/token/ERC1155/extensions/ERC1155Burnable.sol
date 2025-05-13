3326d8bc87bd8eae1c0b8ed754f7762820e1871a
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:51:39 +0500

    Update ERC1155Burnable.sol

--- contracts/token/ERC1155/extensions/ERC1155Burnable.sol
+++ contracts/token/ERC1155/extensions/ERC1155Burnable.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC1155/extensions/ERC1155Burnable.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {ERC1155} from "../ERC1155.sol";
 
