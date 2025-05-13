62fde997798ec90b37a932ac515a52d9cb16ef09
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:00:03 +0500

    Update IERC3156.sol

--- contracts/interfaces/IERC3156.sol
+++ contracts/interfaces/IERC3156.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC3156.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IERC3156FlashBorrower} from "./IERC3156FlashBorrower.sol";
 import {IERC3156FlashLender} from "./IERC3156FlashLender.sol";
