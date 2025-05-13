dd25d5aee6c8b84a7c5de4c976ae6739bff852c7
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:24:03 +0500

    Update draft-ERC6909ContentURI.sol

--- contracts/token/ERC6909/extensions/draft-ERC6909ContentURI.sol
+++ contracts/token/ERC6909/extensions/draft-ERC6909ContentURI.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (token/ERC6909/extensions/draft-ERC6909ContentURI.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {ERC6909} from "../draft-ERC6909.sol";
 import {IERC6909ContentURI} from "../../../interfaces/draft-IERC6909.sol";
