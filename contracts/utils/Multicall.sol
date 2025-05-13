66574c737c07d8d40452e7610e17d55cb60a1aa5
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:48:31 +0500

    Update Multicall.sol

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (utils/Multicall.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {Address} from "./Address.sol";
 import {Context} from "./Context.sol";
