3a9c4a1c04c4703b4d848e9b8bc20f21e6528251
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:38:29 +0500

    Update MerkleProof.sol

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (utils/cryptography/MerkleProof.sol)
 // This file was procedurally generated from scripts/generate/templates/MerkleProof.js.
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {Hashes} from "./Hashes.sol";
 
