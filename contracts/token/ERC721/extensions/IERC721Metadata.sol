878d32685045d917f8aa1dc59fc73a13953cd6d5
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:28:05 +0500

    Update IERC721Metadata.sol

--- contracts/token/ERC721/extensions/IERC721Metadata.sol
+++ contracts/token/ERC721/extensions/IERC721Metadata.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC721/extensions/IERC721Metadata.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IERC721} from "../IERC721.sol";
 
