0fee69f1e01efd34170ccce94828a267fc8c3d87
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:36:33 +0500

    Update AccessManager.sol

--- contracts/access/manager/AccessManager.sol
+++ contracts/access/manager/AccessManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (access/manager/AccessManager.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IAccessManager} from "./IAccessManager.sol";
 import {IAccessManaged} from "./IAccessManaged.sol";
