28cbbffe5e4c56820a1e4aac69f872170dd85fa5
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:49:03 +0500

    Update NoncesKeyed.sol

--- contracts/utils/NoncesKeyed.sol
+++ contracts/utils/NoncesKeyed.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.2.0) (utils/NoncesKeyed.sol)
-pragma solidity ^0.8.20;
+
+pragma solidity ^0.8.10;
 
 import {Nonces} from "./Nonces.sol";
 
