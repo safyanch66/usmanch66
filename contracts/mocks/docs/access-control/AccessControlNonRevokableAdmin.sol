c0d256894922dbcf8a68ad389dfb2b5caf01375d
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:18:54 +0500

    Update AccessControlNonRevokableAdmin.sol

--- contracts/mocks/docs/access-control/AccessControlNonRevokableAdmin.sol
+++ contracts/mocks/docs/access-control/AccessControlNonRevokableAdmin.sol
@@ -1,6 +1,6 @@
 // contracts/AccessControlNonRevokableAdmin.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {AccessControl} from "../../../access/AccessControl.sol";
 
