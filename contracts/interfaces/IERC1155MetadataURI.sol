c8a12d3e8c991ee3a40a830bf3e3b90fcd095777
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:50:54 +0500

    Update IERC1155MetadataURI.sol

--- contracts/interfaces/IERC1155MetadataURI.sol
+++ contracts/interfaces/IERC1155MetadataURI.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC1155MetadataURI.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IERC1155MetadataURI} from "../token/ERC1155/extensions/IERC1155MetadataURI.sol";
