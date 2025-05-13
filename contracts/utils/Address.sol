8b7f70bc0f26f357a852b67c675723014547c0a0
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:45:26 +0500

    Update Address.sol

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.2.0) (utils/Address.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {Errors} from "./Errors.sol";
 
