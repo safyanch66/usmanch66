fdc8456f7bf2b3537ac0d13c726f8f70913034ba
safyanch66 <safyanch88@gmail.com>
2025-05-13 13:20:38 +0500

    Update ERC20Wrapper.sol

--- contracts/token/ERC20/extensions/ERC20Wrapper.sol
+++ contracts/token/ERC20/extensions/ERC20Wrapper.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (token/ERC20/extensions/ERC20Wrapper.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IERC20, IERC20Metadata, ERC20} from "../ERC20.sol";
 import {SafeERC20} from "../utils/SafeERC20.sol";
