0c62f270abee2f1968bc9c0968476d17283e98bd
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:52:03 +0500

    Update IERC1363Receiver.sol

--- contracts/interfaces/IERC1363Receiver.sol
+++ contracts/interfaces/IERC1363Receiver.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (interfaces/IERC1363Receiver.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 /**
  * @title IERC1363Receiver
