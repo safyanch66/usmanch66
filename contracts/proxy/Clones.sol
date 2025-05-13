d02ef211dd877561bc4a14f4189ce2e10686f69e
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:50:34 +0500

    Update Clones.sol

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.3.0) (proxy/Clones.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {Create2} from "../utils/Create2.sol";
 import {Errors} from "../utils/Errors.sol";
