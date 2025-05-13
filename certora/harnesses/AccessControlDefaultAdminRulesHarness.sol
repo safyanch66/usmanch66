8b0a1bf3bfef50bc84a1b39ea660b497fde87661
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:18:24 +0500

    Update AccessControlDefaultAdminRulesHarness.sol

--- certora/harnesses/AccessControlDefaultAdminRulesHarness.sol
+++ certora/harnesses/AccessControlDefaultAdminRulesHarness.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {AccessControlDefaultAdminRules} from "../patched/access/extensions/AccessControlDefaultAdminRules.sol";
 
