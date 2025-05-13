18113b5d3ee8ebc551c7eb17c68367029ed60036
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:51:12 +0500

    Update IERC1155Receiver.sol

--- contracts/interfaces/IERC1155Receiver.sol
+++ contracts/interfaces/IERC1155Receiver.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC1155Receiver.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IERC1155Receiver} from "../token/ERC1155/IERC1155Receiver.sol";
