5c84586c862a6be4ec92809d050dd0bcfca6cb16
safyanch66 <safyanch88@gmail.com>
2025-05-13 11:30:28 +0500

    Update AccessControlEnumerable.sol

--- contracts/access/extensions/AccessControlEnumerable.sol
+++ contracts/access/extensions/AccessControlEnumerable.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (access/extensions/AccessControlEnumerable.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {IAccessControlEnumerable} from "./IAccessControlEnumerable.sol";
 import {AccessControl} from "../AccessControl.sol";
