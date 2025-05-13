89e3e549b35ce400528531ed842ee71de4bdab60
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:48:23 +0500

    Update ERC1967Proxy.sol

--- contracts/proxy/ERC1967/ERC1967Proxy.sol
+++ contracts/proxy/ERC1967/ERC1967Proxy.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.2.0) (proxy/ERC1967/ERC1967Proxy.sol)
 
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.12;
 
 import {Proxy} from "../Proxy.sol";
 import {ERC1967Utils} from "./ERC1967Utils.sol";
