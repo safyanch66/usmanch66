1c578944617e7b97fc143e3a983fc2e4f24af759
safyanch66 <safyanch88@gmail.com>
2025-05-13 12:37:15 +0500

    Update AccessManagedTarget.sol

--- contracts/mocks/AccessManagedTarget.sol
+++ contracts/mocks/AccessManagedTarget.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 
 import {AccessManaged} from "../access/manager/AccessManaged.sol";
 import {StorageSlot} from "../utils/StorageSlot.sol";
